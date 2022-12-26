d46512c4f691eae76bafccc947df5900012c9258
Stewie <estebancanal25@gmail.com>
2022-12-26 17:25:27 +0100

    Sin NoMVC

--- NoMVC/PHP/header.php
+++
@@ -1,6 +0,0 @@
-<div id="menu" style="background-color:grey">
-    <a href="posts.php">Posts</a> | <a href="login.php">Login</a>
-    <?php if (isset($_SESSION["currentuser"])) : ?>
-        <div style="float:right">Hello <?= $_SESSION["currentuser"] ?> <a href="logout.php">(Logout)</a></div>
-    <?php endif ?>
-</div>
