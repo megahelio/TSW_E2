c9751b5176d1a48d9d12ddf5c83900e8234b7f1b
Shinig4mi0mega <61286403+Shinig4mi0mega@users.noreply.github.com>
2022-12-30 11:17:30 +0100

    Update UserRest.php
    Se pueden añadir usuarios pero la parte de validar la veré cuando se haga en el front, porque creo que me tiene que llegar algo de ahí

--- Entrega2-SPAsaveYourMoney/rest/UserRest.php
+++ Entrega2-SPAsaveYourMoney/rest/UserRest.php
@@ -1,50 +1,67 @@
 <?php
 
-require_once(__DIR__."/../model/User.php");
-require_once(__DIR__."/../model/UserMapper.php");
-require_once(__DIR__."/BaseRest.php");
+require_once(__DIR__ . "/../model/User.php");
+require_once(__DIR__ . "/../model/UserMapper.php");
+require_once(__DIR__ . "/BaseRest.php");
 
 /**
-* Class UserRest
-*
-* It contains operations for adding and check users credentials.
-* Methods gives responses following Restful standards. Methods of this class
-* are intended to be mapped as callbacks using the URIDispatcher class.
-*
-*/
-class UserRest extends BaseRest {
+ * Class UserRest
+ *
+ * It contains operations for adding and check users credentials.
+ * Methods gives responses following Restful standards. Methods of this class
+ * are intended to be mapped as callbacks using the URIDispatcher class.
+ *
+ */
+class UserRest extends BaseRest
+{
 	private $userMapper;
 
-	public function __construct() {
+	public function __construct()
+	{
 		parent::__construct();
 
 		$this->userMapper = new UserMapper();
 	}
 
-	public function postUser($data) {
-		$user = new User($data->username, $data->password);
+	public function postUser($data)
+	{
+		$user = new User();
+		$user->setUsername($data->username);
+		$user->setEmail( $data->email);
+		$user->setPassword($data->passwd);
+		$user->setPasswordbis($data->passwdbis);
+
+
+		print_r($data);
+		print_r($user);
+
 		try {
-			$user->checkIsValidForRegister();
+			$user->checkIsValidForRegister(); // if it fails, ValidationException
+		} catch (ValidationException $ex) {
+			// Get the errors array inside the exepction...
+			$errors = $ex->getErrors();
+			// And put it to the view as "errors" variable
+			print_r($errors);
+		}
 
+		$user->setPassword(md5($data->passwd));
+		// check if user exists in the database
+		if (!$this->userMapper->usernameExists($data->username)) {
+
+			// save the User object into the database
 			$this->userMapper->save($user);
-
-			header($_SERVER['SERVER_PROTOCOL'].' 201 Created');
-			header("Location: ".$_SERVER['REQUEST_URI']."/".$data->username);
-		}catch(ValidationException $e) {
-			http_response_code(400);
-			header('Content-Type: application/json');
-			echo(json_encode($e->getErrors()));
 		}
 	}
 
-	public function login($username) {
+	public function login($data)
+	{
 		$currentLogged = parent::authenticateUser();
-		if ($currentLogged->getUsername() != $username) {
-			header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
-			echo("You are not authorized to login as anyone but you");
+		if ($currentLogged->getUsername() != $data) {
+			header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden');
+			echo ("You are not authorized to login as anyone but you");
 		} else {
-			header($_SERVER['SERVER_PROTOCOL'].' 200 Ok');
-			echo("Hello ".$username);
+			header($_SERVER['SERVER_PROTOCOL'] . ' 200 Ok');
+			echo ("Hello " . $data);
 		}
 	}
 }
@@ -52,5 +69,5 @@ class UserRest extends BaseRest {
 // URI-MAPPING for this Rest endpoint
 $userRest = new UserRest();
 URIDispatcher::getInstance()
-->map("GET",	"/user/$1", array($userRest,"login"))
-->map("POST", "/user", array($userRest,"postUser"));
+	->map("GET",	"/user/$1", array($userRest, "login"))
+	->map("POST", "/user", array($userRest, "postUser"));
