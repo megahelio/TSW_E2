99051e2964aa147c4990bc858553a11617e62f6b
megahelio <47276914+megahelio@users.noreply.github.com>
2022-12-31 20:12:35 +0100

    Update updateGasto($data)
    Pienso que se explica en los comentarios de la función. Lo único resaltable es el funcionamiento del update.
    
    Si el campo llega vacío -> Dejar el valor de antes
    
    Si un campo obligatorio llega con un valor -> Validar -> Actualizar
    
    Si un campo opcional (uuidFichero) llega con un valor -> Es "REMOVE" ? Enviar el campo vacío para eliminar el valor antiguo : (Si no es "REMOVE") Validar el dato y actualizar. [Líneas 291-300]

--- Entrega2-SPAsaveYourMoney/rest/GastoRest.php
+++ Entrega2-SPAsaveYourMoney/rest/GastoRest.php
@@ -13,7 +13,7 @@ require_once(__DIR__ . "/BaseRest.php");
  * Class GastoRest
  *
  * It contains operations for creating, retrieving, updating, deleting and
- * listing gastos, as well as to create comments to gastos.
+ * listing gastos.
  *
  * Methods gives responses following Restful standards. Methods of this class
  * are intended to be mapped as callbacks using the URIDispatcher class.
@@ -30,7 +30,8 @@ class GastoRest extends BaseRest
         $this->gastoMapper = new GastoMapper();
     }
     /**
-     * Emite los gastos del usuario logeado o Forbiden
+     * Emite los gastos del usuario logeado
+     * @throws  401 Unauthorized -> no hay usuario logeado
      * @return json gastos del usuario logeado
      */
     public function getGastos()
@@ -69,7 +70,7 @@ class GastoRest extends BaseRest
      * @throws  401 Unauthorized -> no hay usuario logeado
      * @throws  403 Forbidden -> existe el id pero el usuario logeado es distinto al propietario del gasto
      * 
-     * @param string $data id
+     * @param $data id
      * 
      * @return 200 OK + json información del gasto -> existe el id y el usuario logeado es el propietario del gasto
      */
@@ -116,13 +117,14 @@ class GastoRest extends BaseRest
      * @throws  401 Unauthorized -> No hay usuario logeado
      * @throws  500 Internal Server Error -> GastoMapper no devuelve el gasto creado
      * 
-     * @param Gasto $data gasto a guardar
+     * @param $data gasto a guardar
      * 
      * @return 200 OK + json información del gasto -> se crea el gasto correctamente
      */
 
     public function createGasto($data)
     {
+
         $currentUser = parent::authenticateUser()->getUsername();
 
         //parseamos Parametro a objeto
@@ -204,10 +206,10 @@ class GastoRest extends BaseRest
                     "uuidFichero" => $gastoSaved->getUuidFichero()
 
                 )));
-            //el gasto no se crea correctamente
+                //si el gasto no se crea correctamente...
             } else {
                 header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error');
-                die("Gasto was not created by a server error.");
+                die("Gasto might be not created by a server error.");
             }
         } catch (ValidationException $e) {
             header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad request');
@@ -216,50 +218,125 @@ class GastoRest extends BaseRest
         }
     }
 
-
+    /**
+     * Edita el gasto especificado.
+     * 
+     * @param int gastoId -> id del gasto a modificar
+     * @param Gasto data -> Información del nuevo gasto
+     * 
+     * @throws 400 Bad request -> No existe ningún gasto con esa id
+     * @throws 400 Bad request -> La solicitud no contiene campos obligatorios
+     * @throws  401 Unauthorized -> No hay usuario logeado
+     * @throws 403 Forbidden -> El gasto no pertenece al usuario logueado
+     * @throws  500 Internal Server Error -> GastoMapper no devuelve el gasto actualizado
+     * 
+     * @return 200 OK + json información del gasto -> se actualiza el gasto correctamente
+     */
     public function updateGasto($gastoId, $data)
     {
-        //$currentUser = parent::authenticateUser();
+
+
+        $currentUser = parent::authenticateUser()->getUsername();
 
         $gasto = $this->gastoMapper->findGastoById($gastoId);
         if ($gasto == NULL) {
             header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad request');
-            echo ("Gasto with id " . $gastoId . " not found");
-            return;
+            die("Gasto with id " . $gastoId . " not found");
         }
 
-        // // Check if the Gasto author is the currentUser (in Session)
-        // if ($gasto->getAuthor() != $currentUser) {
-        //     header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden');
-        //     echo ("you are not the author of this gasto");
-        //     return;
-        // }
+        // Check if the Gasto owner is the currentUser (in Session)
+        if ($gasto->getUsuario()->getUsername() != $currentUser) {
+            header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden');
+            die("You(" . $currentUser . ") must be the owner of the gasto");
+        }
 
         $gastoUpdate = new Gasto();
 
+        /* Para poblar el objeto que se subirá a la base de datos
+        a diferencia de la funcionalidad de crear, en el que el usuario introducía los valores finales y estos no podían ser nulos,
+        para la funcionalidad de editar el json que se suministra podrá contener datos vacíos. Esto significará una instrucción de "no edición",
+        "preservar el valor antiguo".
+        En el caso de campos que puedan ser nulos (no obligatorios) como el caso de el fichero usaremos REMOVE para indicar que el nuevo valor debe ser nulo. */
+
         $gastoUpdate->setId($gastoId);
-        $gastoUpdate->setTipo($data->tipo);
-        $gastoUpdate->setCantidad($data->cantidad);
-        $gastoUpdate->setFecha($data->fecha);
-        $gastoUpdate->setDescription($data->descripcion);
-        $gastoUpdate->setUuidFichero($data->UuidFichero);
+
+        $gastoUpdate->setUsuario($currentUser);
+
+        //Campo obligatorio
+        if (isset($data->tipo)) {
+            $gastoUpdate->setTipo($data->tipo);
+        } else {
+            $gastoUpdate->setTipo($gasto->getTipo());
+        }
+
+        if (isset($data->cantidad)) {
+            $gastoUpdate->setCantidad($data->cantidad);
+        } else {
+            $gastoUpdate->setCantidad($gasto->getCantidad());
+        }
+
+        //El formato de la fecha 1999-12-31 funciona
+        if (isset($data->fecha)) {
+            $gastoUpdate->setFecha($data->fecha);
+        } else {
+            $gastoUpdate->setFecha($gasto->getFecha());
+        }
+
+        if (isset($data->descripcion)) {
+            $gastoUpdate->setDescription($data->descripcion);
+        } else {
+            $gastoUpdate->setDescription($gasto->getDescription());
+        }
+
+        //Campo no obligatorio.
+        if (isset($data->uuidfichero)) {
+            if ($data->uuidfichero == "REMOVE") {
+                $gastoUpdate->setUuidFichero(null);
+            } else {
+                $gastoUpdate->setUuidFichero($data->uuidfichero);
+            }
+        } else {
+            $gastoUpdate->setUuidFichero($gasto->getUuidFichero());
+        }
 
         try {
             // validate Gasto object
-            //$gasto->checkIsValidForAdd(); // if it fails, ValidationException
+            $gastoUpdate->checkIsValidForAdd(); // if it fails, ValidationException
             $this->gastoMapper->update($gastoUpdate);
-            header($_SERVER['SERVER_PROTOCOL'] . ' 200 Ok');
+            $gastoSaved = $this->gastoMapper->findGastoById($gastoId);//recuperamos el gasto editado
+            
+            if (isset($gastoSaved)) {
+                header($_SERVER['SERVER_PROTOCOL'] . ' 200 Ok');
+                header('Content-Type: application/json');
+                echo (json_encode(array(
+                    "id" => $gastoSaved->getId(),
+                    "usuario" => $gastoSaved->getUsuario()->getUsername(),
+                    "tipo" => $gastoSaved->getTipo(),
+                    "cantidad" => $gastoSaved->getCantidad(),
+                    "fecha" => $gastoSaved->getFecha(),
+                    "description" => $gastoSaved->getDescription(),
+                    "uuidFichero" => $gastoSaved->getUuidFichero()
+
+                )));
+                //si el gasto no se actualiza correctamente...
+            } else {
+                header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error');
+                die("Gasto might be not updated by a server error.");
+            }
+            
         } catch (ValidationException $e) {
             header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad request');
             header('Content-Type: application/json');
-            echo (json_encode($e->getErrors()));
+            die(json_encode($e->getErrors()));
         }
     }
     /**
      * Elimina el gasto con el id que se pasa como parámetro
+     * 
      * @throws  400 Bad request -> no existe el id
      * @throws  401 Unauthorized -> no hay usuario logeado
      * @throws  403 Forbidden -> existe el id pero el usuario logeado es distinto al propietario del gasto
+     * 
      * @param string $data id
      * 
      * @return 204 No Content -> existe el id y el usuario logeado es el propietario del gasto
@@ -279,7 +356,7 @@ class GastoRest extends BaseRest
         //Check if the Gasto author is the currentUser (in Session)
         if ($gasto->getUsuario()->getUsername() != $currentUser) {
             header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden');
-            die("You (" . $currentUser . ") are not the author of this gasto.");
+            die("You (" . $currentUser . ") are not the owner of this gasto.");
         }
 
         $this->gastoMapper->delete($gasto);
