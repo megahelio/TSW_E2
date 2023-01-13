952c960cafc39d6894edcddd92f98ef805a50547
megahelio <47276914+megahelio@users.noreply.github.com>
2023-01-13 22:22:46 +0100

    Delete post items

--- Entrega2-SPAsaveYourMoney/js/app.js
+++ Entrega2-SPAsaveYourMoney/js/app.js
@@ -33,14 +33,6 @@ Promise.all([
     Handlebars.templates.user = Handlebars.compile(source)),
   loadTextFile('templates/components/login.hbs').then((source) =>
     Handlebars.templates.login = Handlebars.compile(source)),
-  loadTextFile('templates/components/posts-table.hbs').then((source) =>
-    Handlebars.templates.poststable = Handlebars.compile(source)),
-  loadTextFile('templates/components/post-edit.hbs').then((source) =>
-    Handlebars.templates.postedit = Handlebars.compile(source)),
-  loadTextFile('templates/components/post-view.hbs').then((source) =>
-    Handlebars.templates.postview = Handlebars.compile(source)),
-  loadTextFile('templates/components/post-row.hbs').then((source) =>
-    Handlebars.templates.postrow = Handlebars.compile(source)),
   loadTextFile('templates/components/gasto-edit.hbs').then((source) =>
     Handlebars.templates.gastoedit = Handlebars.compile(source)),
   loadTextFile('templates/components/gasto-row.hbs').then((source) =>
